927f3beced0f94a357397787548ac50e3dcc9613
ehg <chris.blower@jumpwire.co.uk>
2011-07-03 17:39:52 +0100

    initial commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0.rc4'
+
+# Bundle edge Rails instead:
+# gem 'rails',     :git => 'git://github.com/rails/rails.git'
+
+gem 'sqlite3'
+
+# Asset template engines
+gem 'json'
+gem 'sass-rails', "~> 3.1.0.rc"
+gem 'coffee-script'
+gem 'uglifier'
+
+gem 'jquery-rails'
+
+group :development, :test do
+  gem "rspec-rails", ">= 2.0.0"
+  gem "cucumber-rails"
+  gem "capybara"
+  gem "database_cleaner"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Privydo::Application.config.secret_token = '172556f3db77fae16c36faebdaf65e944ea4f389281c03b2c75ad1eb4806c181bbdcbed7e3629386d206fd9e9e1ee237bd74d01be7f19abf78e30df34f7ca707'
