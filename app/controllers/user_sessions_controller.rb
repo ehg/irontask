54c1acbe09cde7b327ab37aa344f6c3f1f8b638d
ehg <chris.blower@jumpwire.co.uk>
2011-10-11 20:29:45 +0100

    Flash an error not a notice. Tidy up a bit.

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,28 +1,25 @@
 class UserSessionsController < ApplicationController
-  
+
   def new
   end
-  
+
   def create
-      authenticate!
-
-      if logged_in?
-        flash[:notice] = "Login successful!"
-        redirect_to :controller => :tasks, :action => :index 
-      end
+    authenticate!
+    if logged_in?
+      flash[:notice] = "Login successful!"
+      redirect_to :controller => :tasks, :action => :index 
     end
+  end
 
-    def destroy
-      logout
+  def destroy
+    logout
+    flash[:notice] = "You have signed out."
+    redirect_to new_session_path
+  end
 
-      flash[:notice] = "You have signed out."
-      redirect_to new_session_path
-    end
-
-    def unauthenticated
-      flash[:notice] = "Wrong Username/Email and password combination."
-
-      redirect_to new_session_path
-      return false
-    end
+  def unauthenticated
+    flash[:error] = "Wrong Username/Email and password combination."
+    redirect_to new_session_path
+    return false
+  end
 end
